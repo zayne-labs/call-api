09a61e5556af598e99f02ba54aa9b99842f49109
Ryan Zayne <phoenixastro97@gmail.com>
2024-06-19 20:12:58 +0100

    .

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
 	format: ["esm", "cjs"],
-	entry: ["src/index.ts", "src/utils.ts", "src/createFetchClient.ts"],
+	entry: ["src/index.ts"],
 	target: "es2022",
 	clean: true, // clean up dist folder,
 	skipNodeModulesBundle: true, // skip building deps for node_modules, simply use them as is
