9190d6a4f31303c35fe9595ca32d5059b40abd61
Ryan Zayne <phoenixastro97@gmail.com>
2024-06-26 00:25:09 +0100

    build: add separate configs for esm and cjs, with diff folders

---
+++ .changeset/tame-tips-nail.md
@@ -0,0 +1,5 @@
+---
+"@zayne-labs/callapi": patch
+---
+
+add separate configs for esm and cjs, with diff folders
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,31 @@
-import { defineConfig } from "tsup";
+import { type Options, defineConfig } from "tsup";
 
-export default defineConfig((options) => ({
-	format: ["esm", "cjs"],
-	entry: ["src/index.ts", "src/createFetchClient.ts", "src/utils.ts", "src/typeof.ts"],
-	target: "es2022",
-	clean: true, // clean up dist folder,
-	skipNodeModulesBundle: true, // skip building deps for node_modules, simply use them as is
-	dts: true,
-	minify: !options.watch,
-	splitting: true,
-	bundle: false,
-	treeshake: true,
-	tsconfig: "tsconfig.json",
-	sourcemap: !options.watch,
-}));
+export default defineConfig((options) => {
+	const commonOptions = {
+		clean: true, // clean up dist folder,
+		dts: true,
+		minify: !options.watch,
+		tsconfig: "tsconfig.json",
+		sourcemap: !options.watch,
+	} satisfies Options;
+
+	return [
+		{
+			...commonOptions,
+			format: ["esm"],
+			entry: ["src/index.ts", "src/createFetchClient.ts", "src/utils.ts", "src/typeof.ts"],
+			target: "esnext",
+			outDir: "./dist/esm",
+			// bundle: false,
+			skipNodeModulesBundle: true, // skip building deps for node_modules, simply use them as is
+			splitting: true,
+			treeshake: true,
+		},
+		{
+			...commonOptions,
+			entry: ["src/index.ts"],
+			format: ["cjs"],
+			outDir: "./dist/cjs",
+		},
+	];
+});
