96dbc575f3eccce77efc7f04bbd6612c8e48d59f
ryan-zayne <phoenixastro97@gmail.com>
2024-10-29 11:22:00 +0100

    feat(createFetchClient): allow TBaseResultMode to be undefined
    - Allows the `TBaseResultMode` generic type parameter in the `createFetchClient` function to be `undefined`, which will default to `ResultModeUnion`.
    - This provides more flexibility in the usage of the `createFetchClient` function, allowing users to omit the `TBaseResultMode` type parameter if they want to use the base result mode.
    - Updates the `callApi` function to use the `TBaseResultMode` as the default for the `TResultMode` parameter when it is not provided.

--- src/createFetchClient.ts
+++ src/createFetchClient.ts
@@ -28,7 +28,7 @@ import { isFunction, isObject, isQueryString, isString } from "./utils/typeof";
 export const createFetchClient = <
 	TBaseData,
 	TBaseErrorData = unknown,
-	TBaseResultMode extends ResultModeUnion = ResultModeUnion,
+	TBaseResultMode extends ResultModeUnion = undefined,
 >(
 	baseConfig: BaseCallApiConfig<TBaseData, TBaseErrorData, TBaseResultMode> = {}
 ) => {
@@ -59,7 +59,7 @@ export const createFetchClient = <
 	const callApi = async <
 		TData = TBaseData,
 		TErrorData = TBaseErrorData,
-		TResultMode extends ResultModeUnion = undefined,
+		TResultMode extends ResultModeUnion = TBaseResultMode,
 	>(
 		url: string,
 		config: CallApiConfig<TData, TErrorData, TResultMode> = {}
