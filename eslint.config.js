75618655ab683bec829ba82b0fa1bb5076ce818c
Ryan-Zayne <phoenixastro97@gmail.com>
2024-11-10 13:42:22 +0100

    fix(eslint): simplify tsconfigPath to include all tsconfig.json files fix(package): update lint:eslint script to build before linting

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,6 @@ export default zayne({
 	type: "lib",
 	ignores: ["dist/**", "eslint.config.js"],
 	typescript: {
-		tsconfigPath: [
-			"dev/tsconfig.json",
-			"packages/callapi/tsconfig.json",
-			"packages/callapi-legacy/tsconfig.json",
-			"docs/tsconfig.json",
-		],
+		tsconfigPath: ["**/tsconfig.json"],
 	},
 });
