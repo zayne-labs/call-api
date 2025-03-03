e7d0ed8d44b553a2772146bb9b064159f8a1b3fc
Ryan-Zayne <phoenixastro97@gmail.com>
2025-03-03 01:21:03 +0100

    feat: add schema-dts and implement schema.org markup for SEO
    This commit introduces the `schema-dts` package as a dependency and implements schema.org markup using JSON-LD to improve SEO for the documentation site.
    
    The changes include:
    
    - Adding `schema-dts` as a dependency to `apps/docs/package.json`.
    - Implementing a `WebSite` schema in `apps/docs/app/(home)/layout.tsx` to provide structured data about the website to search engines.
    - Adding a `<script>` tag with `application/ld+json` type to inject the JSON-LD data into the page.

--- apps/docs/app/(home)/layout.tsx
+++ apps/docs/app/(home)/layout.tsx
@@ -1,5 +1,22 @@
+import type { WebSite, WithContext } from "schema-dts";
+
+const jsonLd: WithContext<WebSite> = {
+	"@context": "https://schema.org",
+	"@type": "WebSite",
+	image: "https://zayne-labs-callapi.netlify.app/og.png",
+	name: "CallApi",
+	url: "https://zayne-labs-callapi.netlify.app",
+};
+
 function HomeLayout({ children }: { children: React.ReactNode }) {
-	return children;
+	return (
+		<>
+			{/* eslint-disable-next-line react-dom/no-dangerously-set-innerhtml -- It's fine  */}
+			<script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+
+			{children}
+		</>
+	);
 }
 
 export default HomeLayout;
